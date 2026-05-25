d2fe84c75e7d3fc6d72bd32227d35c6ddb5583d9
saebuabu <a.saebu@kw1c.nl>
2026-05-25 23:57:52 +0200

    querySelectort en click-event

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
-console.log('📚 Mijn Bibliotheek - Server is gestart!');
+console.log('app.js is geladen!');
+const titel = document.querySelector('#titel');
+
+console.log(`waarde van titel: ${titel.value}`);
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log(`waarde van titel: ${titel.value}`);
+});
