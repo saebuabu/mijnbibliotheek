e699e8f4797a5a7bd848c470dd20a883f18bee8a
saebuabu <a.saebu@kw1c.nl>
2026-05-25 23:24:07 +0200

    toevoegen toegevoegd aan project

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.get('/dashboard', (req, res) => {
    res.render('dashboard', { title: 'Dashboard - Mijn Bibliotheek', appNaam: 'Mijn Bibliotheek' });
 });
 
+app.get('/toevoegen', (req, res) => {
+   res.render('toevoegen', { title: 'Toevoegen', appNaam: 'Mijn Bibliotheek' });
+});
+
 // Stap 3: Server starten
 app.listen(PORT, () => {
     console.log(`✅ Server draait op http://localhost:${PORT}`);
