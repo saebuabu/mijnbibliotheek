9486bdb0b8b4a0defd5dbc2c392aacfa0a8cbb23
saebuabu <a.saebu@kw1c.nl>
2026-06-01 11:27:54 +0200

    voorbeelden van event listeners toegevoegd aan formulier

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,23 @@
 console.log('app.js is geladen!');
+// eerste stap: elementen selecteren
 const titel = document.querySelector('#titel');
-
-console.log(`waarde van titel: ${titel.value}`);
-
 const btn = document.querySelector('#btn');
+
+// tweede stap: event listener toevoegen
 btn.addEventListener('click', (event) => {
+    // derde stap: default gedrag voorkomen
     event.preventDefault();
-    console.log(`waarde van titel: ${titel.value}`);
+    // vierde stap: actie uitvoeren
+    console.log(`submit button clicked!`);
+});
+
+//voorbeeld input event listener, toont het aantal ingevoerde tekens in het titel veld
+titel.addEventListener('input', () => {
+    console.log(`Aantal tekens in titel: ${titel.value.length}`);
+});
+
+//voorbeeld  change event listener van de select, toont de geselecteerde waarde
+const genre = document.querySelector('#genre');
+genre.addEventListener('change', () => {
+    console.log(`Geselecteerd genre: ${genre.value}`);
 });
